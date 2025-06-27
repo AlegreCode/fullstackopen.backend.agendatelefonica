c00cb8972c1eb67b0a6cdb8d50821d30d5c949ff
Carlos Alberto Alegre <ca.alegre269@gmail.com>
2025-06-27 20:00:31 -0300

    fix: update endpoint methods to use mongoose Person schema

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const app = express()
 const morgan = require('morgan')
 const cors = require('cors')
+const Person = require('./db/schema')
 
 require('./db/connect')
 
@@ -22,75 +23,67 @@ app.use(morgan('tiny'))
 app.use(cors())
 app.use(express.static('dist'))
 
-let persons = [
-    { 
-      "id": 1,
-      "name": "Arto Hellas", 
-      "number": "040-123456"
-    },
-    { 
-      "id": 2,
-      "name": "Ada Lovelace", 
-      "number": "39-44-5323523"
-    },
-    { 
-      "id": 3,
-      "name": "Dan Abramov", 
-      "number": "12-43-234345"
-    },
-    { 
-      "id": 4,
-      "name": "Mary Poppendieck", 
-      "number": "39-23-6423122"
-    }
-]
+// let persons = [
+//     { 
+//       "id": 1,
+//       "name": "Arto Hellas", 
+//       "number": "040-123456"
+//     },
+//     { 
+//       "id": 2,
+//       "name": "Ada Lovelace", 
+//       "number": "39-44-5323523"
+//     },
+//     { 
+//       "id": 3,
+//       "name": "Dan Abramov", 
+//       "number": "12-43-234345"
+//     },
+//     { 
+//       "id": 4,
+//       "name": "Mary Poppendieck", 
+//       "number": "39-23-6423122"
+//     }
+// ]
 
-app.get('/api/persons', (request, response) => {
-  response.json(persons)
+app.get('/api/persons', async (request, response) => {
+    const persons = await Person.find()
+    response.json(persons)
 })
 
-app.get('/api/persons/:id', (request, response) => {
-    const id = Number(request.params.id)
-    const person = persons.find(person => person.id === id)
-    if (person) {
-      response.json(person)
-    } else {
-      response.status(404).end()
-    }
+app.get('/api/persons/:id', async (request, response) => {
+    const id = request.params.id
+    const person = await Person.findById(id)
+    response.json(person)
 })
 
-app.delete('/api/persons/:id', (request, response) => {
-    const id = Number(request.params.id)
-    persons = persons.filter(person => person.id !== id)
+app.delete('/api/persons/:id', async (request, response) => {
+    const id = request.params.id
+    await Person.findByIdAndDelete(id)
     response.status(204).end()
 })
 
-const generateId = () => {
-    return Math.max(...persons.map(person => person.id)) + 1
-}
-
-app.post('/api/persons', (request, response) => {
+app.post('/api/persons', async (request, response) => {
     const body = request.body
     if (!body.name || !body.number) {
       return response.status(400).json({ error: 'name or number is missing' })
     }
 
-    const existingPerson = persons.find(person => person.name === body.name)
+    const existingPerson = await Person.findOne({ name: body.name })
     if (existingPerson) {
         return response.status(400).json({ error: 'name must be unique' })
     }
 
-    const person = {
-      id: generateId(),
+    const person = new Person({
       name: body.name,
       number: body.number,
-    }
-    persons = persons.concat(person)
-    response.json(person)
+    })
+    const savedPerson = await person.save()
+    response.json(savedPerson)
 })
 
-app.get("/info", (request, response) => {
-    const template = `Phonebook has info for ${persons.length} people <br> ${new Date()}`
+app.get("/info", async (request, response) => {
+    const template = `Phonebook has info for ${Person.countDocuments()} people <br> ${new Date()}`
     response.send(template)
 })
 
